4b68cf5370937bbb6d1cc7848068fb8474321a88
aaituu <farhatadil1719@gmail.com>
2025-11-16 15:52:35 +0500

    Delete sdp_final directory

--- sdp_final/src/decorator/DocumentProcessor.java
+++
@@ -1,7 +0,0 @@
-package decorator;
-
-import model.Document;
-
-public interface DocumentProcessor {
-    Document process(Document document);
-}
--- sdp_final/src/factory/ConverterFactory.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-import strategy.ConversionStrategy;
-
-public abstract class ConverterFactory {
-    public abstract ConversionStrategy createConverter(String inputFormat, String outputFormat);
-
-    public abstract boolean supportsConversion(String inputFormat, String outputFormat);
-}
